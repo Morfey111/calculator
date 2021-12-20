7908c675605b20d3597330d4bddc0c496d701f5f
Morfey111 <boris.marinov.marinov@gmail.com>
2021-12-20 12:39:40 +0200

    App is already

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,9 +1,12 @@
+
 import { ACTIONS } from "./App"
 
 export default function OperationButton({ dispatch, operation }) {
   return (
     <button
-      onClick={() => dispatch({ type: ACTIONS.CHOSE_OPERATION, payload: { operation } })}
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
     >
       {operation}
     </button>
